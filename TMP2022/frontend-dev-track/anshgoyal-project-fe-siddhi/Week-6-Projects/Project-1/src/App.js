ab58fad9193f25518df90c3a89639e87936c7d93
Ansh Goyal <anshgoyal1704@gmail.com>
2022-10-24 23:16:49 +0530

    Add files via upload

---
+++ TMP2022/frontend-dev-track/anshgoyal-project-fe-siddhi/Week-6-Projects/Project-1/src/App.js
@@ -0,0 +1,14 @@
+import BodyItems from "./BodyItems";
+import Navigation from "./Navigation";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <BodyItems/>
+    </div>
+  );
+}
+
+export default App;
