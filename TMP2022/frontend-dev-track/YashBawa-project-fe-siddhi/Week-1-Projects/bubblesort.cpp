c511a7a76597a531c98c6a9cf97ae5445316d37f
unknown <YashBawa>
2022-11-01 17:48:33 +0530

    Final projects

--- TMP2022/frontend-dev-track/YashBawa-project-fe-siddhi/Week-1-Projects/bubblesort.cpp
+++ TMP2022/frontend-dev-track/YashBawa-project-fe-siddhi/Week-1-Projects/bubblesort.cpp
@@ -6,7 +6,7 @@ void bubbleSort(int arr[], int n)
 {
 	int i, j;
 	for (i = 0; i < n - 1; i++)
-		for (j = 0; j < n - i - 1; j++)
+		for (j = 0; j < n -  i - 1; j++)
 			if (arr[j] > arr[j + 1])
 				swap(arr[j], arr[j + 1]);
 }
