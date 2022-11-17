1f8a94c52f7f0d27105e15ea9c5371107a4ec949
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2022-11-17 20:06:39 +0530

    Update App.js

--- TMP2022/frontend-dev-track/anjali-project-fe-siddhi/Final Project/src/App.js
+++ TMP2022/frontend-dev-track/anjali-project-fe-siddhi/Final Project/src/App.js
@@ -4,7 +4,17 @@ import ProjectList from './Components/Project/ProjectList';
 function App() {
   return (
     <div className="App">
-     <ProjectList/>
+      <div>
+        <h1>
+          Techrity Mentorship Final Project - All Techrity Projects Website
+        </h1>
+        <hr />
+        <h3>
+          This is a solution to the Final Project of Techrity Frontend
+          Development Mentorship.
+        </h3>
+      </div>
+      <ProjectList />
     </div>
   );
 }
