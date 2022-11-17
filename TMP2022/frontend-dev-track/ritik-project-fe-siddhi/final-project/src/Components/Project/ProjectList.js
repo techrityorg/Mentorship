2b060ef2c3db556f22f879bb1b008f806a2526e7
Ritik Kumar <srivastavaritik360@gmail.com>
2022-11-17 14:26:57 +0530

    updated deployed links for projects

--- TMP2022/frontend-dev-track/ritik-project-fe-siddhi/final-project/src/Components/Project/ProjectList.js
+++ TMP2022/frontend-dev-track/ritik-project-fe-siddhi/final-project/src/Components/Project/ProjectList.js
@@ -30,6 +30,7 @@ const ProjectList = () => {
             image={W2p1}
             title="Biodata Page"
             subtitle="This is a solution to the Week-2: Project-1 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-2/Project-1/index.html"
           />
         </div>
         <div className="PL_display">
@@ -37,6 +38,7 @@ const ProjectList = () => {
             image={W2p2}
             title="Registration Form"
             subtitle="This is a solution to the Week-2: Project-2 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-2/Project-2/index.html"
           />
         </div>
         <div className="PL_display">
@@ -44,6 +46,7 @@ const ProjectList = () => {
             image={W2p3}
             title="Table of HTML Tags"
             subtitle="This is a solution to the Week-2: Project-3 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-2/Project-3/index.html"
           />
         </div>{" "}
         <div className="PL_display">
@@ -51,6 +54,7 @@ const ProjectList = () => {
             image={W2p4}
             title="School Website"
             subtitle="This is a solution to the Week-2: Project-4 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-2/Project-4/index.html"
           />
         </div>
       </div>
@@ -61,6 +65,7 @@ const ProjectList = () => {
             image={W3p1}
             title="Form"
             subtitle="This is a solution to the Week-3: Project-1 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-3/Project-1/index.html"
           />
         </div>
         <div className="PL_display">
@@ -68,6 +73,7 @@ const ProjectList = () => {
             image={W3p2}
             title="Responsive Profile Card"
             subtitle="This is a solution to the Week-3: Project-2 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-3/Project-2/index.html"
           />
         </div>
         <div className="PL_display">
@@ -75,6 +81,7 @@ const ProjectList = () => {
             image={W3p3}
             title="HTML CSS Carousel"
             subtitle="This is a solution to the Week-3: Project-3 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-3/Project-3/index.html"
           />
         </div>{" "}
         <div className="PL_display">
@@ -82,6 +89,7 @@ const ProjectList = () => {
             image={W3p4}
             title="Responsive School Website"
             subtitle="This is a solution to the Week-3: Project-4 of Techrity Frontend Development Mentorship. "
+            visit="https://tda.ritikk.me/week-3/Project-4/index.html"
           />
         </div>
       </div>
@@ -92,6 +100,7 @@ const ProjectList = () => {
             image={W4p1}
             title="Age Calculator"
             subtitle="This is a solution to the Week-4: Project-1 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-4/Project-1/index.html"
           />
         </div>
         <div className="PL_display">
@@ -99,6 +108,7 @@ const ProjectList = () => {
             image={W4p2}
             title="JavaScript Calculator"
             subtitle="This is a solution to the Week-4: Project-2 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-4/Project-2/index.html"
           />
         </div>
         <div className="PL_display">
@@ -106,6 +116,7 @@ const ProjectList = () => {
             image={W4p3}
             title="Form"
             subtitle="This is a solution to the Week-4: Project-3 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-4/Project-3/index.html"
           />
         </div>{" "}
       </div>
@@ -116,6 +127,7 @@ const ProjectList = () => {
             image={W5p1}
             title="Tic Tac Toe Game"
             subtitle="This is a solution to the Week-5: Project-1 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-5/Project-1/index.html"
           />
         </div>
         <div className="PL_display">
@@ -123,6 +135,7 @@ const ProjectList = () => {
             image={W5p2}
             title="Weather App"
             subtitle="This is a solution to the Week-5: Project-2 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-5/Project-2/index.html"
           />
         </div>
         <div className="PL_display">
@@ -130,6 +143,7 @@ const ProjectList = () => {
             image={W5p3}
             title="Password Generator"
             subtitle="This is a solution to the Week-5: Project-3 of Techrity Frontend Development Mentorship."
+            visit="https://tda.ritikk.me/week-5/Project-3/index.html"
           />
         </div>{" "}
       </div>
@@ -140,6 +154,7 @@ const ProjectList = () => {
             image={W2p1}
             title="Portfolio Website in ReactJS"
             subtitle="This is a solution to the Week-6: Project-1&2 of Techrity Frontend Development Mentorship."
+            visit="https://techrity-w6p1-2.vercel.app/"
           />
         </div>
         <div className="PL_display">
@@ -147,6 +162,7 @@ const ProjectList = () => {
             image="https://user-images.githubusercontent.com/78131705/193799037-9e3f0824-3fd2-4476-8314-01e25a62e65e.png"
             title="Weather App in ReactJS"
             subtitle="This is a solution to the Week-6: Project-3 of Techrity Frontend Development Mentorship."
+            visit="https://techrity-w6p3.vercel.app/"
           />
         </div>
       </div>
